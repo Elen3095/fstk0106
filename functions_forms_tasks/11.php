62eb9383b98c54c35ce0239651bffb5189fed2d4
Elen3095 <alena3095@ukr.net>
2017-07-19 20:46:05 +0300

    use mb_substr and mb_strtoupper

--- functions_forms_tasks/11.php
+++ functions_forms_tasks/11.php
@@ -13,16 +13,24 @@
 <?php
 
 function Caps($getString){
-    $arr = explode('. ',$getString);
-//    print_r($arr);
     $arr2=[];
-        foreach ($arr as $value){
-            $value=ucfirst($value);
-//            print_r($value);
-            array_push($arr2,$value);
-        }
+    $arr = explode('. ',$getString);
+    foreach ($arr as $value){
+        $value = mb_strtoupper(mb_substr($value, 0, 1)).mb_substr($value, 1, strlen($value)-1);
+//        echo $value;
+        array_push($arr2, $value);
+
+    }
+//    print_r($arr);
+//    $arr2=[];
+//        foreach ($arr as $value){
+//            $value=ucfirst($value);
+////            print_r($value);
+//            array_push($arr2,$value);
+//        }
 //
-$m=implode('. ',$arr2);
+//$m=implode('. ',$arr2);
+    $m=implode('. ',$arr2);
 echo $m;
 }
 if($_SERVER['REQUEST_METHOD']=='POST'){
