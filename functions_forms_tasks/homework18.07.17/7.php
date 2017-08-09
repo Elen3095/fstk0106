b87386cb24892a94f4aeae08702c2cf290761105
Elen3095 <alena3095@ukr.net>
2017-08-09 12:57:47 +0300

    7.php

---
+++ functions_forms_tasks/homework18.07.17/7.php
@@ -0,0 +1,56 @@
+<!--Создать гостевую книгу, где любой человек может оставить комментарий в текстовом поле и добавить его.-->
+<!--Все добавленные комментарии выводятся над текстовым полем.-->
+
+<?php
+$arrayOfComments=$arrayOfUsers=$arrayOfCommentsToShow=[];
+$seeComment="";
+
+if($_SERVER['REQUEST_METHOD']=='POST'&&isset($_POST['textToComment'])&&isset($_POST['name'])){
+    $coment=$_POST['textToComment'];
+    $userName=$_POST['name'];
+    $fileName=__DIR__.DIRECTORY_SEPARATOR.$userName.'.txt';
+//
+    $fullTextComment=$userName." : ".$coment;
+//    $arrayOfComments[$userName]=$coment;
+    $seeComment.=" ".$fullTextComment;
+    file_put_contents($fileName, $seeComment);
+//    array_push($arrayOfCommentsToShow, $arrayOfComments);
+//    print_r($arrayOfCommentsToShow) ;
+
+
+//    array_push($arrayOfComments,$coment);
+//    array_push($arrayOfUsers,$userName);
+//    foreach ($arrayOfUsers as $key=>$value){
+//        $commentsToShow="<br />"."{$arrayOfUsers[$key]}".":"."<br />"."{$arrayOfComments[$key]}";
+//        array_push($arrayOfCommentsToShow, $commentsToShow);
+//    }
+//    $seeComment=serialize($arrayOfCommentsToShow);
+}
+foreach ()
+?>
+
+
+<div>
+    Гостевая книга
+</div>
+<div>
+    <?php
+    print_r(file_get_contents($fileName));
+
+    ?>
+</div>
+<div>
+    <form method="post">
+        <input type="text" name="name">
+        <br>
+        <textarea name="textToComment">
+
+        </textarea>
+        <br>
+        <input type="submit" value="Добавить коммент">
+    </form>
+
+</div>
+
+
+
